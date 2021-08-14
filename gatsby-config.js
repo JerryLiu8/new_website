28e68680cbe09285d0e105ce043d7d928f25f321
Jerry Liu <jerryliu800@gmail.com>
2021-08-14 19:56:16 -0400

    cleaning files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: ``,
-    description: `Jerry Liu Personal Portfolio Website`,
+    title: `Personal Portfolio`,
+    description: `Jerry Liu's Personal Portfolio Website`,
     author: `Jerry Liu`,
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
--- src/yourdata.js
+++ src/yourdata.js
@@ -1,9 +1,5 @@
 // Skills Icons
-import htmlIcon from "./images/html.svg"
-import cssIcon from "./images/css.svg"
 import reactIcon from "./images/react.svg"
-import designIcon from "./images/design.svg"
-import codeIcon from "./images/code.svg"
 import pythonIcon from "./images/python.svg"
 import javaIcon from "./images/java.svg"
 import cppIcon from "./images/cpp.svg"
@@ -81,16 +77,6 @@ export default {
       url: "https://jerryliu8.github.io/index.html",
     },
 
-
-    /*
-    If You Want To Add More Project just Copy and Paste This At The End
-    ,{
-        title: 'Project Five',
-        para: 'Something Amazing',
-        imageSrc: "",
-        url: ''
-    }
-    */
   ],
 
   // End Projects Section -----------------------
@@ -145,4 +131,3 @@ export default {
   // End Contact Section ---------------
 }
 
-// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
